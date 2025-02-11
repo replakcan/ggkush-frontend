13b72c356c5ffcfecba57fdd1e7135914fcd9afd
replakcan <alper.makcan@gmail.com>
2025-02-11 22:52:44 +0300

    Updates main with routes and providers

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./App.css";
+import App from "./App.jsx";
+import { Provider } from "react-redux";
+import store from "./store/store.js";
+import { PrimeReactProvider } from "primereact/api";
+import { BrowserRouter, Route, Routes } from "react-router";
+import PeopleLayout from "./layout/PeopleLayout.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <PrimeReactProvider>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+            <Route path="/people" element={<PeopleLayout />}/>
+
+            <Route path="/" element={<App />}/>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  </PrimeReactProvider>
+);
