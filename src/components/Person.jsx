9562a18ac26a0b8e2af362369297c7969e15a2d7
replakcan <alper.makcan@gmail.com>
2025-03-04 10:36:13 +0300

    Changes Person props

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,14 +1,12 @@
 /* eslint-disable react/prop-types */
 import { Card } from "primereact/card";
 
-const Person = ({ firstName, lastName, nickName, email }) => {
-  const fullName = `${firstName} ${lastName}`;
+const Person = ({ userName, nickName }) => {
   nickName = "@" + nickName.replaceAll(" ", "_");
 
   return (
-    <Card className="mx-8 my-3" title={fullName}>
+    <Card className="mx-8 my-3" title={userName}>
       <p>{nickName}</p>
-      <p className="p-0">{email}</p>
     </Card>
   );
 };
