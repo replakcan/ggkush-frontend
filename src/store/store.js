c114c14ea8629072d5a8c87a6074f2833dbb4442
replakcan <alper.makcan@gmail.com>
2025-03-04 10:34:18 +0300

    Updates store with redux add-on

--- src/store/store.js
+++ src/store/store.js
@@ -1,14 +1,26 @@
-import { legacy_createStore as createStore, combineReducers, applyMiddleware } from "redux"
-import logger from "redux-logger"
-import { thunk } from "redux-thunk"
-import TweetReducer from "./reducers/tweetReducer";
-import UserReducer from "./reducers/userReducer";
-
-const rootReducer = combineReducers({
+import {
+    legacy_createStore as createStore,
+    combineReducers,
+    applyMiddleware,
+    compose,
+  } from "redux";
+  import logger from "redux-logger";
+  import { thunk } from "redux-thunk";
+  import TweetReducer from "./reducers/tweetReducer";
+  import UserReducer from "./reducers/userReducer";
+  
+  const rootReducer = combineReducers({
     user: UserReducer,
-    tweet: TweetReducer
-})
-
-const store = createStore(rootReducer, applyMiddleware(thunk, logger));
-
-export default store;
+    tweet: TweetReducer,
+  });
+  
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  
+  const store = createStore(
+    rootReducer,
+    composeEnhancers(applyMiddleware(thunk, logger))
+  );
+  
+  export default store;
+  
