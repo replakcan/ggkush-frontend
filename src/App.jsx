70b2fe2fa138827b294b3f0dd7618b8b81ed6294
replakcan <alper.makcan@gmail.com>
2025-03-04 10:35:19 +0300

    Adds login and register routes on app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import 'primereact/resources/themes/lara-light-indigo/theme.css';
-import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css';
-import 'primeflex/primeflex.css';
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
 
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { fetchTweets } from "./store/actions/tweetActions";
-import { fetchUsers } from "./store/actions/userActions";
 import { Route, Routes } from "react-router";
 import TweetLayout from "./layout/TweetLayout";
 import PeopleLayout from "./layout/PeopleLayout";
-import Home from "./pages/Home"
-import Login from './pages/Login';
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchUsers());
-    dispatch(fetchTweets());
-  }, [dispatch]);
 
   return (
-    <>
+    <main>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/tweets" element={<TweetLayout />} />
         <Route path="/people" element={<PeopleLayout />} />
-        <Route path='/login' element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
-    </>
+    </main>
   );
 }
 
